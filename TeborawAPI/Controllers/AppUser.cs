46b54b7bfbd2e1c6a15520c082ed8bed63475459
Rompasaurus <michael.e.mchenry@gmail.com>
2024-01-19 15:55:59 +0100

    added postgre ef nuget pkg and prelim entity

---
+++ TeborawAPI/Controllers/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace TeborawAPI;
+
+public class AppUser
+{
+    public int Id { get; set; }
+    public string UserName {get; set;}
+    
+
+}
--- TeborawAPI/obj/Debug/net8.0/TeborawAPI.AssemblyInfo.cs
+++ TeborawAPI/obj/Debug/net8.0/TeborawAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeborawAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdd824e318b8855e869cf4ea6e8cca82e8b60bff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeborawAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeborawAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
