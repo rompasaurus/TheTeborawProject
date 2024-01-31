a51eac7ba119c03f8edf2f7ae1f9a7227dbe4a20
Rompasaurus <michael.e.mchenry@gmail.com>
2024-01-31 17:59:10 +0100

    fixed the null interest and looking for made the props nullable not sure why that happended in the first place created migration to confer these changes

---
+++ TeborawAPI/Migrations/20240131165710_fixcols.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeborawAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixcols : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
